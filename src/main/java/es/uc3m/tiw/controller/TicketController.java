8846c93d1f6e3131a0c20551fa48e79957560a68
Brafit2001 <100429071@alumnos.uc3m.es>
2022-12-08 20:08:55 +0100

    cambios

--- src/main/java/es/uc3m/tiw/controller/TicketController.java
+++ src/main/java/es/uc3m/tiw/controller/TicketController.java
@@ -34,9 +34,6 @@ public class TicketController {
 		List<Ticket> ticketList;
 		try {
 			ticketList = daoti.findAll();
-			if (ticketList.isEmpty()){
-				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-			}
 		}catch (Exception e){
 			e.printStackTrace();
 			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
